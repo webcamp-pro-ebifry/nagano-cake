e114b31d84cc1f07aadd136a3ddaf8433a959941
檜澤ゆい <yuihizawa@hinokisawayuinoMacBook-Air.local>
2020-07-04 13:59:18 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,19 @@
 Rails.application.routes.draw do
   devise_for :client_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  get 'admin/homes/top' => 'admin/homes#top'
+  get 'homes/top' => 'homes#top'
 
-  resources :users
+  resources :products
+  resources :genres
+  resources :shipping_adress
+  resources :cart_items
+  resources :orders
+  resources :order_details
+
+ 
 
 
-	get	'/client_users/' =>'client_users#show'
-	get	'/client_users/edit'=> 'client_users#edit'
-	get '/client_users/confirmation' => 'client_users#confirmation'
-
-
-
-	get '/products/' => 'client_user/products#index'
-	get '/products/:id' => 'client_user/products#show'
-
-	get '/admin/products/:id/edit' =>'admin/products#edit'
-	patch	'/adminproducts/:id/update' =>'admin/products#update'
-	get	'/admin/products/new' => 'admin/products#new'
-	post	'/admin/products/:id/create' =>	'admin/products#create'
-	get	'/admin/products/ '	=> 'admin/products#index'
 
 
 
